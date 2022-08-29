c0f9cea05cb3a98904aaf4bbbc3239e1f7c37549
Nguyễn Công Hậu <conghau1132000@gmail.com>
2022-08-29 21:17:59 +0700

    setup project

---
+++ src/redux/reducer.js
@@ -0,0 +1,26 @@
+const initState = {
+  filters: {
+    search: "",
+    status: "All",
+    priority: [],
+  },
+  todoList: [
+    { id: 1, name: "Learn yoga", completed: false, priority: "Medium" },
+    { id: 2, name: "Learn Redux", completed: true, priority: "High" },
+    { id: 3, name: "Learn JavaScript", completed: false, priority: "low" },
+  ],
+};
+
+const rootReducer = (state = initState, action) => {
+  switch (action.type) {
+    case "todoList/addTodo":
+      return {
+        ...state,
+        todoList: [...state.todoList, action.payload],
+      };
+    default:
+      return state;
+  }
+};
+
+export default rootReducer
